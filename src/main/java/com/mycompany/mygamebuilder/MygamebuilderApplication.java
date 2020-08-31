c4c15f2143a2b0f143e7b6feb6caec46b209dbc4
Ivan Franchin <ivan.franchin@takeaway.com>
2020-08-31 09:09:31 +0200

    add project inside a spring-boot app that will be the server

---
+++ src/main/java/com/mycompany/mygamebuilder/MygamebuilderApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.mygamebuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MygamebuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MygamebuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycompany/mygamebuilder/MygamebuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.mygamebuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MygamebuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
