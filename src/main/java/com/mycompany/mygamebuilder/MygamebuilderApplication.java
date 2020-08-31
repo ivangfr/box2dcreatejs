d8fc5ee5994fbc217e8fbb11a94b2200b88ed680
Ivan Franchin <ivan.franchin@takeaway.com>
2020-08-31 10:55:54 +0200

    remove IndexController and fix indentation

--- src/main/java/com/mycompany/mygamebuilder/MygamebuilderApplication.java
+++ src/main/java/com/mycompany/mygamebuilder/MygamebuilderApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MygamebuilderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MygamebuilderApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MygamebuilderApplication.class, args);
+    }
 
 }
--- src/test/java/com/mycompany/mygamebuilder/MygamebuilderApplicationTests.java
+++ src/test/java/com/mycompany/mygamebuilder/MygamebuilderApplicationTests.java
@@ -1,13 +1,15 @@
 package com.mycompany.mygamebuilder;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class MygamebuilderApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
